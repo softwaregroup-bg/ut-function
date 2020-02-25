4be5b91ebe06a068efd956bb8d894adde9c76bb1
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-02-26 10:17:21 +1100

    fix: cbc decrypt bad value handling

--- packages/cbc/index.js
+++ packages/cbc/index.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-module.exports = key => {
+module.exports = (key, validate) => {
     const iv = Buffer.alloc(16);
     crypto.randomFillSync(iv);
 
@@ -13,7 +13,15 @@ module.exports = key => {
     const pad = s => s + ' '.repeat(16 - s.length % 16);
 
     return {
-        encrypt: val => Buffer.concat([enc.update(crypto.randomFillSync(iv)), enc.update(pad(val))]),
-        decrypt: val => dec.update(Buffer.concat([Buffer.from(val, 'hex'), iv])).toString('utf8', 32).trim()
+        encrypt: value => Buffer.concat([enc.update(crypto.randomFillSync(iv)), enc.update(pad(value))]),
+        decrypt: value => {
+            value = Buffer.from(value, 'hex');
+            if (validate && value.length % 16) {
+                const error = new Error('Length of encrypted value is not multiple of 16');
+                error.type = 'utFunction.cbc.decrypt';
+                throw error;
+            }
+            return dec.update(Buffer.concat([value, iv.slice(value.length % 16)])).toString('utf8', 32).trim();
+        }
     };
 };
--- packages/cbc/index.test.js
+++ packages/cbc/index.test.js
@@ -2,11 +2,13 @@ const tap = require('tap');
 const key = '757435736f66747761726567726f7570757435736f66747761726567726f7570';
 const {encrypt: encrypt1, decrypt: decrypt1} = require('.')(key);
 const {encrypt: encrypt2, decrypt: decrypt2} = require('.')(Buffer.from(key, 'hex'));
-
+const {decrypt} = require('.')(key, true);
 tap.test('decrypt', assert => {
     assert.matchSnapshot(decrypt1(encrypt1('test1')), 'decrypt1 buffer');
     assert.matchSnapshot(decrypt1(encrypt1('test1').toString('hex')), 'decrypt1 hex');
     assert.matchSnapshot(decrypt2(encrypt2('test2')), 'decrypt2 buffer');
+    decrypt2('00'); // try to unsync with bad length
     assert.matchSnapshot(decrypt2(encrypt2('test2').toString('hex')), 'decrypt2 hex');
+    assert.throws(() => decrypt('00'), {type: 'utFunction.cbc.decrypt'}, 'decrypt validate bad length');
     assert.end();
 });
