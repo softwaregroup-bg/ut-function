fc5d4f3cf0c98e2aa24ad07aaf7b835c879d9c79
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-07-22 13:49:39 +0300

    feat: auto namespace

--- packages/dispatch/index.js
+++ packages/dispatch/index.js
@@ -1,19 +1,30 @@
 const scriptPort = require('ut-port-script');
 module.exports = function({namespace, methods}) {
+    let namespaces = namespace;
+    if (!namespaces) {
+        methods = arguments[0];
+        namespaces = Array.from(
+            new Set(
+                Object.keys(methods).map(name => name.split('.', 2)[0])));
+    };
+    if (!namespaces.length) throw new Error('Missing namespace for dispatch');
     return (...params) => ({
-        [namespace]: class extends scriptPort(...params) {
+        [namespaces[0]]: class extends scriptPort(...params) {
             get defaults() {
                 return {
-                    namespace
+                    namespace: namespaces
                 };
             }
 
             handlers() {
                 return {
-                    [namespace + 'service.get']: () => params[0].utMethod.pkg,
+                    ...namespaces.reduce((prev, name) => ({
+                        ...prev,
+                        [name + 'service.get']: () => params[0].utMethod.pkg
+                    }), {}),
                     ...methods
                 };
             }
         }
-    }[namespace]);
+    }[namespaces[0]]);
 };
