fb229246cce5360f02641e4a65839f3d41bbb041
plamen.dimitrov <plamen.dimitrov@softwaregroup.com>
2020-09-02 15:25:57 +0300

    feat:Adding custom parser for the needs of the DFA projects.

--- packages/xml2json/index.js
+++ packages/xml2json/index.js
@@ -1,28 +1,8 @@
-const flatten = require('ut-function.flatten');
-const template = require('ut-function.template');
-const set = require('lodash.set');
-const get = require('lodash.get');
-const xml2js = require('xml2js');
-const xmlParser = new xml2js.Parser({
-    charkey: 'text',
-    mergeAttrs: true,
-    explicitArray: false,
-    tagNameProcessors: [xml2js.processors.stripPrefix]
-});
+const xmlParser = require('./xmlParser').parse;
 
 module.exports = (xmlTemplate, maxDepth = 50) => {
-    const fn = (async() => template(
-        JSON.stringify(
-            Object
-                .entries(flatten(await xmlParser.parseStringPromise(xmlTemplate), maxDepth))
-                .reduce((prev, [name, value]) => {
-                    const path = value.match(/^\${(.*)}$/);
-                    if (path) set(prev, path[1], '$' + `{ut.get(xml, '${name}')}`);
-                    return prev;
-                }, {})
-        ), ['xml'], {get}))();
-    return async(xml, json) => {
-        const result = (await fn)(typeof xml === 'string' ? await xmlParser.parseStringPromise(xml) : await xml);
-        return json ? result : JSON.parse(result);
+    return async(xml, xmlTemplatePath) => {
+        const result = await xmlParser(xmlTemplatePath, xml);
+        return typeof result === 'object' ? result : JSON.parse(result);
     };
 };
