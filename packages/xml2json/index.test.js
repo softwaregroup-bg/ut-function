ca4c4bea2809cebe912e78d66b42d74c0f685622
plamen.dimitrov <plamen.dimitrov@softwaregroup.com>
2020-09-03 12:30:08 +0300

    fix:Adopt tests to new functionality.

--- packages/xml2json/index.test.js
+++ packages/xml2json/index.test.js
@@ -1,22 +1,12 @@
-const sortKeys = require('sort-keys');
 const path = require('path');
 const tap = require('tap');
 const fs = require('fs');
 const parse = require('./')(fs.readFileSync(path.resolve(__dirname, 'test', 'template.xml')).toString());
+const templatePath = (path.resolve(__dirname, 'test', 'template.xml').toString());
 const payload = fs.readFileSync(path.resolve(__dirname, 'test', 'payload.xml')).toString();
 
 tap.test('parse', async assert => {
-    assert.matchSnapshot(sortKeys(await parse(payload)), 'parse xml string to object');
-    assert.matchSnapshot(await parse(payload, true), 'parse xml string to JSON');
-    assert.matchSnapshot(sortKeys(await parse({
-        some: {
-            deep: {
-                nested: {
-                    structure: 'value 1'
-                }
-            },
-            veryVeryLongTagName: 'value 2'
-        }
-    })), 'extract object from parsed xml');
-    assert.end();
+    const wantedResult = { 'a': 'value 1', 'b': { 'c': 'value 2' }, 'd': undefined };
+    assert.deepEqual(await parse(payload, templatePath), wantedResult, 'parse xml string to JSON');
+    // assert.end();
 });
--- packages/xml2json/xmlParser.js
+++ packages/xml2json/xmlParser.js
@@ -1,30 +1,27 @@
-var xml2js = require('xml2js');
-var fs = require('fs');
-//var util = require('util');
-var DOMParser = require('xmldom').DOMParser;
-
-//var dataFile = fs.readFileSync('./listCustomer_resp.xml').toString();
+const xml2js = require('xml2js');
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
 
 function getPath(ob, p, attributes) {
     if (ob.parentNode) {
         p.push(ob.localName);
         getPath(ob.parentNode, p, attributes);
-        var possibleNamesStr = ob.getAttribute('possibleNames');
-        if (possibleNamesStr != '') {
+        const possibleNamesStr = ob.getAttribute('possibleNames');
+        if (possibleNamesStr !== '') {
             attributes.possNames[ob.localName] = possibleNamesStr.split(',');
         }
-        var explArrAttr = ob.getAttribute('explicitArray');
-        if (explArrAttr != '' && explArrAttr == 'true') {
+        const explArrAttr = ob.getAttribute('explicitArray');
+        if (explArrAttr !== '' && explArrAttr === 'true') {
             attributes.explicitArray = true;
         }
     }
 }
 
 function removeNilValues(ob) {
-    if (typeof (ob) == 'object') {
-        var obKeys = Object.keys(ob);
-        for (var k in obKeys) {
-            if (typeof(obKeys[k]) == 'string' && ob[obKeys[k]]['i:nil'] && ob[obKeys[k]]['i:nil'] == 'true') {
+    if (typeof (ob) === 'object') {
+        const obKeys = Object.keys(ob);
+        for (const k in obKeys) {
+            if (typeof (obKeys[k]) === 'string' && ob[obKeys[k]]['i:nil'] && ob[obKeys[k]]['i:nil'] === 'true') {
                 return '';
             }
         }
@@ -33,18 +30,17 @@ function removeNilValues(ob) {
 }
 
 function getMaping(ob, fieldMapings) {
-
     if (ob.childNodes) {
-        var keys = Object.keys(ob.childNodes);
+        const keys = Object.keys(ob.childNodes);
         keys.forEach(function(pName, i) {
-            if (pName != 'length') {
-                var ob1 = ob.childNodes[pName];
-                if (ob1.nodeName == 'include') {
-                    var tmplPath = ob1.getAttribute('templatePath');
+            if (pName !== 'length') {
+                let ob1 = ob.childNodes[pName];
+                if (ob1.nodeName === 'include') {
+                    const tmplPath = ob1.getAttribute('templatePath');
                     if (tmplPath) {
-                        var templString = fs.readFileSync(require.resolve(tmplPath)).toString();
-                        var templDoc = new DOMParser().parseFromString(templString);
-                        ob.replaceChild(templDoc.firstChild, ob1)
+                        const templString = fs.readFileSync(require.resolve(tmplPath)).toString();
+                        const templDoc = new DOMParser().parseFromString(templString);
+                        ob.replaceChild(templDoc.firstChild, ob1);
                         ob1 = ob.childNodes[i];
                     }
                 }
@@ -52,47 +48,43 @@ function getMaping(ob, fieldMapings) {
             }
         });
     } else {
-        var nodeValue = ob.nodeValue;
-        var pat = /^\$\{(.*)}$/;
-        var res = nodeValue.match(pat);
+        const nodeValue = ob.nodeValue;
+        const pat = /^\$\{(.*)}$/;
+        const res = nodeValue.match(pat);
         if (res && res[1]) {
-            var path = [];
-            var attributes = {possNames: {}, explicitArray: false};
-            var possNames = {};
-            var explicitArray = false;
+            const path = [];
+            const attributes = { possNames: {}, explicitArray: false };
             getPath(ob.parentNode, path, attributes);
             path.reverse();
-            fieldMapings.push({path: path, fldName: res[1], possblNames: attributes.possNames, explicitArray: attributes.explicitArray});
-
+            fieldMapings.push({ path: path, fldName: res[1], possblNames: attributes.possNames, explicitArray: attributes.explicitArray });
         }
     }
 }
 
 function extractValue(obbVal, path, pNames, resultObj, possNames, explicitArray) {
-    var lstName = pNames[(pNames.length - 1)];
+    const lstName = pNames[(pNames.length - 1)];
 
-    for (var i = 1; i <= path.length; i++) {
-        if (obbVal && !Array.isArray(obbVal) && explicitArray && i == (path.length - 1)) {
+    for (let i = 1; i <= path.length; i++) {
+        if (obbVal && !Array.isArray(obbVal) && explicitArray && i === (path.length - 1)) {
             obbVal = [obbVal];
         }
         if (Array.isArray(obbVal)) {
-
-            var childName = path.length == i ? lstName : (pNames.length == 1 ? 'data' : pNames[0]);
+            const childName = path.length === i ? lstName : (pNames.length === 1 ? 'data' : pNames[0]);
             resultObj[childName] = resultObj[childName] || [];
 
-            if ((path.length - 1) == i || path.length == i) {
+            if ((path.length - 1) === i || path.length === i) {
                 obbVal.forEach(function(ar, ix) {
-                    //var val = ar[path[i]] ? ar[path[i]] : ar;
-                    if (typeof(ar[path[i]]) != 'undefined' || (typeof(path[i]) == 'undefined' && typeof(ar) == 'string')) {
-                        // we enter here when there is such property in result object OR when result object i actualy array of string values
-                        var val = removeNilValues(typeof(path[i]) != 'undefined' ? ar[path[i]] : ar);
-                        if (path.length == i) {
+                    //  let val = ar[path[i]] ? ar[path[i]] : ar;
+                    if (typeof (ar[path[i]]) !== 'undefined' || (typeof (path[i]) === 'undefined' && typeof (ar) === 'string')) {
+                        //   we enter here when there is such property in result object OR when result object i actualy array of string values
+                        const val = removeNilValues(typeof (path[i]) !== 'undefined' ? ar[path[i]] : ar);
+                        if (path.length === i) {
                             resultObj[childName].push(val);
-                        }else {
+                        } else {
                             if (resultObj[childName][ix]) {
                                 resultObj[childName][ix][lstName] = val;
                             } else {
-                                var vl = {};
+                                const vl = {};
                                 vl[lstName] = val;
                                 resultObj[childName].push(vl);
                             }
@@ -100,19 +92,19 @@ function extractValue(obbVal, path, pNames, resultObj, possNames, explicitArray)
                     }
                 });
             } else {
-                // todo: ...extract array sub objects
-                //var chOb = extractValue(ar[path[i]], path, pName);
-                //res.push(chOb);
+                //  todo: ...extract array sub objects
+                //  let chOb = extractValue(ar[path[i]], path, pName);
+                //  res.push(chOb);
             }
 
             return resultObj;
-        } else if (typeof(obbVal) == 'object') {
-            var foundPn = false;
+        } else if (typeof (obbVal) === 'object') {
+            let foundPn = false;
             if (possNames[path[i]]) {
-                var posNames = possNames[path[i]];
-                for (var p in posNames) {
-                    if (typeof(posNames[p]) == 'string') {
-                        if (posNames[p] == '*') {
+                const posNames = possNames[path[i]];
+                for (const p in posNames) {
+                    if (typeof (posNames[p]) === 'string') {
+                        if (posNames[p] === '*') {
                             obbVal = obbVal[Object.keys(obbVal)[0]];
                             foundPn = true;
                             break;
@@ -126,71 +118,65 @@ function extractValue(obbVal, path, pNames, resultObj, possNames, explicitArray)
                 }
             }
             if (!foundPn) {
-                if (i == path.length) {
+                if (i === path.length) {
                     resultObj[lstName] = obbVal[path[i]];
-                }else {
+                } else {
                     obbVal = obbVal[path[i]];
                 }
             }
         } else {
-            //text
-            if (pNames.length == 1) {
+            //  text
+            if (pNames.length === 1) {
                 resultObj[lstName] = obbVal;
             } else {
                 if (!obbVal && explicitArray) {
                     resultObj[pNames[0]] = resultObj[pNames[0]] || [];
-                }else {
+                } else {
                     resultObj[pNames[0]] = resultObj[pNames[0]] || {};
                     resultObj[pNames[0]][lstName] = obbVal;
                 }
             }
         }
-
     }
-
 }
 
 function tagNameProcessor(name) {
-
-    var r = name.split(':');
+    const r = name.split(':');
     return r[r.length - 1];
 }
 
-//////////////////////////////////////////////////////
-
+// ----------------------------------------------
 module.exports = {
-
-    parse: function(templName, data){
-
-        var templFile = fs.readFileSync(require.resolve(templName)).toString();
-        var doc = new DOMParser().parseFromString(templFile);
-        var fieldMapings = [];
+    parse: function(templName, data) {
+        const templFile = fs.readFileSync(require.resolve(templName)).toString();
+        const doc = new DOMParser().parseFromString(templFile);
+        const fieldMapings = [];
         getMaping(doc, fieldMapings);
 
         return new Promise(function(resolve, reject) {
-            xml2js.parseString(data, {trim: true,
-                    ignoreAttrs: false,
-                    explicitArray: false,
-                    tagNameProcessors: [tagNameProcessor]
-                },
-                function(err, reslt) {
-                    if(err){
-                        reject(err);
-                        return;
-                    }
-                    var prsRes = {};
-                    for (var k in fieldMapings) {
-                        if(typeof(fieldMapings[k]) == 'object') {
-                            var path = fieldMapings[k].path;
-                            var propNames = fieldMapings[k].fldName.split('.');
-                            var obbVal = reslt[path[0]];
-                            extractValue(obbVal, path, propNames, prsRes, fieldMapings[k].possblNames, fieldMapings[k].explicitArray);
-                        }
-                    }
-                    //console.log(reslt);
-                    resolve(prsRes);
+            xml2js.parseString(data, {
+                trim: true,
+                ignoreAttrs: false,
+                explicitArray: false,
+                tagNameProcessors: [tagNameProcessor]
+            },
+            function(err, reslt) {
+                if (err) {
+                    reject(err);
+                    return;
                 }
-            );
+                const prsRes = {};
+                for (const k in fieldMapings) {
+                    if (typeof (fieldMapings[k]) === 'object') {
+                        const path = fieldMapings[k].path;
+                        const propNames = fieldMapings[k].fldName.split('.');
+                        const obbVal = reslt[path[0]];
+                        extractValue(obbVal, path, propNames, prsRes, fieldMapings[k].possblNames, fieldMapings[k].explicitArray);
+                    }
+                }
+                //    console.log(reslt);
+                resolve(prsRes);
+            });
         });
     }
-}
+};
