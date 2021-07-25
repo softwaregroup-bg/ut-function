f53b46e361aa9ba9e6e445e19c5fc7104faee9c6
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-07-25 15:47:52 +0300

    feat: add orderBy [UTCORE-121]

--- packages/common-joi/index.js
+++ packages/common-joi/index.js
@@ -46,6 +46,10 @@ module.exports = ({
         integer,
         integerNull: integer.allow(null),
         integerRequired: integer.required(),
-        pagination
+        pagination,
+        orderBy: joi.array().items(joi.object().keys({
+            column: joi.string().min(1).max(128),
+            direction: joi.string().valid('ASC', 'DESC')
+        })).optional()
     };
 };
